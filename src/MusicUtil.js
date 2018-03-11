fc32ee1ac121a403fa0be1c91b4e54bbe187d313
Jeffrey Zhou <jeff.s.zhou@gmail.com>
2018-03-10 19:11:59 -0700

    clean up

--- src/MusicUtil.js
+++ src/MusicUtil.js
@@ -21,16 +21,17 @@ import Tone from 'tone';
 //Singleton
 
 const NUM_NOTES_IN_OCTAVE = 12;
+const IONIAN_SCALE = [2, 2, 1, 2, 2, 2, 1];
+
 
 class MusicUtil {
   generateMelody() {
     const synth = new Tone.Synth().toMaster();
     const triggerSynth = (note, duration, time) => this.triggerInstrument(synth, note, duration, time);
-    const range = this.generateNoteRange("A2", "A3", "A2");
+    const range = this.generateNoteRange("A2", "A3", "A2", IONIAN_SCALE);
     Tone.Transport.stop()
     var current_time = Tone.Time("0")
     for (let midi_note of range) {
-      console.log(current_time);
       let sci_note = Tone.Frequency(midi_note, "midi")
       Tone.Transport.schedule(time => triggerSynth(sci_note, '8n', time), current_time.toNotation())
       current_time.add("4n")
@@ -43,34 +44,31 @@ class MusicUtil {
   }
 
   //gives us all possible notes within the given range in the proper key, relative to the given root key
-  generateNoteRange(lowSciNote, highSciNote, Key) {
+  generateNoteRange(lowSciNote, highSciNote, Key, scaleIncrements) {
     const lowNote = Tone.Frequency(lowSciNote).toMidi();
     const highNote = Tone.Frequency(highSciNote).toMidi();
     const rootNote = (Tone.Frequency(Key).toMidi()); 
-
-    const scaleIncrements = [2, 2, 1, 2, 2, 2, 1]; //major scale constant for now
     const numOctaves = Math.ceil((highNote - lowNote) / NUM_NOTES_IN_OCTAVE);
-
-    //generator iterator that, given a starting root note and a scale in the form of increments, expands out the scale for the given number of scales
-    function* getNotes(startRoot, scaleIncrements, numOctaves) {
-      let currentNote = startRoot;
-      let index = 0;
-      while(index < numOctaves * scaleIncrements.length) {
-        let increment = scaleIncrements[index++ % scaleIncrements.length];
-        yield currentNote+=increment;
-      }
-    }
-
     let lowRoot = rootNote;
     while (lowRoot > lowNote) {
       lowRoot -= NUM_NOTES_IN_OCTAVE;
     }
 
-    let notes = [...getNotes(lowRoot, scaleIncrements, numOctaves)];
+    let notes = [...this.generateNotes(lowRoot, scaleIncrements, numOctaves)];
     notes.unshift(lowRoot); //include the lower root
 
     //now filter the notes so we're within the given range
     return notes.filter(note => note >= lowNote && note <= highNote);
+  }
+
+  //generator iterator that, given a starting root note and a scale in the form of increments, expands out the scale for the given number of scale
+  * generateNotes(startRoot, scaleIncrements, numOctaves) {
+    let currentNote = startRoot;
+    let index = 0;
+    while(index < numOctaves * scaleIncrements.length) {
+      let increment = scaleIncrements[index++ % scaleIncrements.length];
+      yield currentNote+=increment;
+    }
   }
 
   generateRandomNote(note_range) {
